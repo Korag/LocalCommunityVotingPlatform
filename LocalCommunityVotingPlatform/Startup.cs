6ccfb421e07cf5114b2c007c9b61fa15bfc95da0
Łukasz Czepielik <koragrex@gmail.com>
2019-05-19 01:52:46 +0200

    Added JWT authentication.

--- LocalCommunityVotingPlatform/Startup.cs
+++ LocalCommunityVotingPlatform/Startup.cs
@@ -1,10 +1,18 @@
+using LocalCommunityVotingPlatform.DAL;
+using LocalCommunityVotingPlatform.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace LocalCommunityVotingPlatform
 {
@@ -20,7 +28,38 @@ namespace LocalCommunityVotingPlatform
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<EntityFrameworkContext>();
+
+            services.AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<EntityFrameworkContext>()
+            .AddDefaultTokenProviders();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+                })
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Configuration["JwtIssuer"],
+                        ValidAudience = Configuration["JwtIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+                        ClockSkew = TimeSpan.Zero 
+                    };
+                });
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSingleton<IDbOperations, DbOperations>();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -30,7 +69,7 @@ namespace LocalCommunityVotingPlatform
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, EntityFrameworkContext context)
         {
             if (env.IsDevelopment())
             {
@@ -46,6 +85,9 @@ namespace LocalCommunityVotingPlatform
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            app.UseAuthentication();
+
 
             app.UseMvc(routes =>
             {
@@ -63,6 +105,8 @@ namespace LocalCommunityVotingPlatform
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
+
+            context.Database.EnsureCreated();
         }
     }
 }

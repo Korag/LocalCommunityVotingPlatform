29d3cc6723f78d5b090d62dc4f82340aff3e9e4e
Łukasz Czepielik <koragrex@gmail.com>
2019-05-19 01:54:13 +0200

    Added AccountController with Register and Login Action and helper CreateJwtToken method.

---
+++ LocalCommunityVotingPlatform/Controllers/AccountController.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using LocalCommunityVotingPlatform.ViewModels;
+using LocalCommunityVotingPlatform.Models;
+using System.Linq;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Certification_System.Controllers
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        //private readonly IEmailSender _emailSender;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            //IEmailSender emailSender,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            //_emailSender = emailSender;
+        }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<string> Login(LoginViewModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginModel.Email, loginModel.Password, isPersistent: false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    var LoggedUser = _userManager.Users.SingleOrDefault(z => z.Email == loginModel.Email);
+                    return await CreateJwtToken(LoggedUser);
+                }
+            }
+
+            return "Login attempt failed";
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ICollection<string> GetAvailableRoles()
+        {
+            List<string> AvailableRoles = new List<string>{"User", "Admin"};
+
+            return AvailableRoles;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<string> Register(RegisterViewModel registeredUser)
+        {
+            if (ModelState.IsValid)
+            {
+                var newUser = new User
+                {
+                    UserName = registeredUser.Email,
+                    Email = registeredUser.Email,
+                    FirstName = registeredUser.FirstName,
+                    LastName = registeredUser.LastName
+                };
+               
+                var result = await _userManager.CreateAsync(newUser, registeredUser.Password);
+
+                if (!await _roleManager.RoleExistsAsync(registeredUser.SelectedRole))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(registeredUser.SelectedRole));
+                }
+
+                var addToRole = await _userManager.AddToRoleAsync(newUser, registeredUser.SelectedRole);
+
+                if (result.Succeeded)
+                {
+                    //to do: send email with email and non hashed password
+                    return "Register attempt succesfull";
+                }
+            }
+
+            return "Register attempt failed";
+        }
+
+        private async Task<string> CreateJwtToken(IdentityUser loggedUser)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, loggedUser.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, loggedUser.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(_configuration["JwtExpireDays"]));
+
+            var roles = await _userManager.GetRolesAsync(loggedUser);
+            claims.AddRange(roles.Select(role => new Claim(ClaimsIdentity.DefaultRoleClaimType, role)));
+
+            var token = new JwtSecurityToken(
+                _configuration["JwtIssuer"],
+                _configuration["JwtIssuer"],
+                claims,
+                expires: expires,
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login), "Account");
+        }
+
+        //[HttpGet]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        //{
+        //    if (userId == null || code == null)
+        //    {
+        //        return RedirectToAction(nameof(HomeController.Index), "Home");
+        //    }
+        //    var user = await _userManager.FindByIdAsync(userId);
+        //    if (user == null)
+        //    {
+        //        throw new ApplicationException($"Unable to load user with ID '{userId}'.");
+        //    }
+        //    var result = await _userManager.ConfirmEmailAsync(user, code);
+        //    return View(result.Succeeded ? "ConfirmEmail" : "Error");
+        //}
+
+
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = await _userManager.FindByEmailAsync(model.Email);
+        //        if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+        //        {
+        //            // Don't reveal that the user does not exist or is not confirmed
+        //            return RedirectToAction(nameof(ForgotPasswordConfirmation));
+        //        }
+
+        //        // For more information on how to enable account confirmation and password reset please
+        //        // visit https://go.microsoft.com/fwlink/?LinkID=532713
+        //        var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+        //        var callbackUrl = Url.ResetPasswordCallbackLink(user.Id, code, Request.Scheme);
+        //        await _emailSender.SendEmailAsync(model.Email, "Reset Password",
+        //           $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
+        //        return RedirectToAction(nameof(ForgotPasswordConfirmation));
+        //    }
+
+        //    // If we got this far, something failed, redisplay form
+        //    return View(model);
+        //}
+
+
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model);
+        //    }
+        //    var user = await _userManager.FindByEmailAsync(model.Email);
+        //    if (user == null)
+        //    {
+        //        // Don't reveal that the user does not exist
+        //        return RedirectToAction(nameof(ResetPasswordConfirmation));
+        //    }
+        //    var result = await _userManager.ResetPasswordAsync(user, model.Code, model.Password);
+        //    if (result.Succeeded)
+        //    {
+        //        return RedirectToAction(nameof(ResetPasswordConfirmation));
+        //    }
+        //    AddErrors(result);
+        //    return View();
+        //}
+
+    }
+}

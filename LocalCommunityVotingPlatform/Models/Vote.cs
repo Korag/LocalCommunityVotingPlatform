5a7b71b36856a9e878dab49c97ba52ca76473fc0
Łukasz Czepielik <koragrex@gmail.com>
2019-05-19 01:52:18 +0200

    Added Vote Model.

---
+++ LocalCommunityVotingPlatform/Models/Vote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalCommunityVotingPlatform.Models
+{
+    public class Vote
+    {
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public string ResolutionId { get; set; }
+        public virtual Resolution Resolution { get; set; }
+    }
+}

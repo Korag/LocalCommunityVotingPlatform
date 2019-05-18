6b4726a47f5bb38aab4443f2cdc0e60e3566c6bd
Łukasz Czepielik <koragrex@gmail.com>
2019-05-19 01:53:26 +0200

    Added RegisterViewModel.

---
+++ LocalCommunityVotingPlatform/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalCommunityVotingPlatform.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public ICollection<string> AvailableRoles { get; set; }
+        public string SelectedRole { get; set; }
+    }
+}

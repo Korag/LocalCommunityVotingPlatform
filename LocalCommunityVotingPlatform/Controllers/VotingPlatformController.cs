bffb3d1ff9bdc8107508fdce5b44a69444f62dd8
Łukasz Czepielik <koragrex@gmail.com>
2019-05-19 01:55:50 +0200

    Added VotingPlatformController with GetUsers [GET] action.

---
+++ LocalCommunityVotingPlatform/Controllers/VotingPlatformController.cs
@@ -0,0 +1,47 @@
+﻿using LocalCommunityVotingPlatform.DAL;
+using LocalCommunityVotingPlatform.Models;
+using LocalCommunityVotingPlatform.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LocalCommunityVotingPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VotingPlatformController : ControllerBase
+    {
+        private readonly UserManager<IdentityRole> _userManager;
+        private IDbOperations _context;
+
+        public VotingPlatformController(UserManager<IdentityRole> userManager)
+        {
+            _context = new DbOperations();
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public ICollection<UserViewModel> GetUsers()
+        {
+            var Users = _context.GetUsers();
+
+            ICollection<UserViewModel> usersView = new List<UserViewModel>();
+
+            foreach (var user in Users)
+            {
+                UserViewModel singleUserView = new UserViewModel
+                {
+                    Email = user.Email,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    //Role =  _userManager.FindByIdAsync(user.Id).Roles.RoleId
+                };
+
+                usersView.Add(singleUserView);
+            }
+
+            return usersView;
+        }
+    }
+}

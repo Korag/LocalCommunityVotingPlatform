b69a2e862034ebbded3298a481d974b29756836d
Łukasz Czepielik <koragrex@gmail.com>
2019-05-19 01:54:37 +0200

    Added Entity Framework Core context class.

---
+++ LocalCommunityVotingPlatform/DAL/EntityFrameworkContext.cs
@@ -0,0 +1,34 @@
+﻿using LocalCommunityVotingPlatform.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalCommunityVotingPlatform.DAL
+{
+    public class EntityFrameworkContext : IdentityDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Resolution> Resolutions { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+
+        public EntityFrameworkContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Database = LocalCommunityVotingPlatformDb; Integrated Security = True");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vote>().HasKey(x => new { x.UserId, x.ResolutionId });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

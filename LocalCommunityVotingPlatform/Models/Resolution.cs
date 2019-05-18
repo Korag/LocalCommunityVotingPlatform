a1e699a5a7e332f7b326ceeac5c95fe106a6d699
Łukasz Czepielik <koragrex@gmail.com>
2019-05-19 01:51:55 +0200

    Added Resolution Model.

---
+++ LocalCommunityVotingPlatform/Models/Resolution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalCommunityVotingPlatform.Models
+{
+    public class Resolution
+    {
+        public string Id { get; set; }
+
+        public virtual ICollection<Vote> Votes { get; set; }
+    }
+}

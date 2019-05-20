98df3101e892e8199dee1ea8fa41ce685fa6c3d3
Łukasz Czepielik <koragrex@gmail.com>
2019-05-20 09:41:59 +0200

    Added AuthorizeComponent.

---
+++ LocalCommunityVotingPlatform/ClientApp/src/components/AuthorizeComponent.js
@@ -0,0 +1,49 @@
+﻿import React, { Component } from 'react';
+import { getJWTtoken } from '../helpers/jwtHandler'
+import { withRouter } from 'react-router-dom'
+import axios from 'axios'
+
+class AuthorizeComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userRole: null
+        };
+    }
+
+    componentDidMount() {
+        const token = getJWTtoken();
+        if (!token) {
+            this.props.history.push('/Login');
+        }
+
+        axios.get('/getUserRole/', {
+            headers: {
+                Authorization: token
+            }
+        }).then(res => this.setState({
+            userRole: res.data
+        })).catch(err => {
+            localStorage.removeItem('jwt_token');
+            this.props.history.push('/Login');
+        });
+    }
+
+    render() {
+        if (this.state.userRole === null) {
+            return (
+                < div > <h1>Waiting for api action..</h1></div>
+            );
+        }
+        if (this.state.userRole == "Admin") {
+            // return admin component
+        }
+        else {
+            // return user component
+        }
+    };
+
+}
+
+export default withRouter(AuthorizeComponent);

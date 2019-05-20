4d29e4f6d534b1b8f8d533b3f6876780e42692a9
Korag <koragrex@gmail.com>
2019-05-21 00:56:13 +0200

    Added ResolutionsComponent.

---
+++ LocalCommunityVotingPlatform/ClientApp/src/components/Resolutions.js
@@ -0,0 +1,9 @@
+﻿import React, { Component } from 'react';
+
+export class Resolutions extends Component {
+    static displayName = Resolutions.name;
+
+    render() {
+        return <div>Uchwały</div>
+    }
+}

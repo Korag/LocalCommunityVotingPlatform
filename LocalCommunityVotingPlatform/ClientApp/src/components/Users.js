504456a79aa039d1b6c7af19a26464596f3aee9a
Korag <koragrex@gmail.com>
2019-05-21 00:55:51 +0200

    Added UsersComponent.

---
+++ LocalCommunityVotingPlatform/ClientApp/src/components/Users.js
@@ -0,0 +1,9 @@
+﻿import React, { Component } from 'react';
+
+export class Users extends Component {
+    static displayName = Users.name;
+
+    render() {
+        return <div>Użytkownicy</div>
+    }
+}

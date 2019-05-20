500c7f7fdaadec10eabba1b993ae28694bdd53bd
Korag <koragrex@gmail.com>
2019-05-21 00:56:02 +0200

    Added MyDataComponent.

---
+++ LocalCommunityVotingPlatform/ClientApp/src/components/MyData.js
@@ -0,0 +1,9 @@
+﻿import React, { Component } from 'react';
+
+export class MyData extends Component {
+    static displayName = MyData.name;
+
+    render() {
+        return <div>Moje dane</div>
+    }
+}

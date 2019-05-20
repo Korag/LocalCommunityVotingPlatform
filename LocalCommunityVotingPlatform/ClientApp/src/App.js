fad0ccd1e972843795f612c565da0f43afeefc5b
Łukasz Czepielik <koragrex@gmail.com>
2019-05-20 09:41:36 +0200

    Changed components tree.

--- LocalCommunityVotingPlatform/ClientApp/src/App.js
+++ LocalCommunityVotingPlatform/ClientApp/src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
+
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
-export default class App extends Component {
-  static displayName = App.name;
+import { Login } from './components/Login';
+import AuthorizeComponent  from './components/AuthorizeComponent'
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faKey } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faEnvelope)
+library.add(faKey)
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Login} />
+                <AuthorizeComponent>
+                    <Route path='/counter' component={Counter} />
+                    <Route path='/fetch-data' component={FetchData} />
+                </AuthorizeComponent>
+            </Layout>
+        );
+    }
 }

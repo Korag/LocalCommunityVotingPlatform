4b6866f06e7f439961356c200f5f5ef7b70a9362
Korag <koragrex@gmail.com>
2019-05-22 19:42:23 +0200

    Added schemas for functions Update and Delete in UsersComponent and also added decission in render() which component should be rendered.

--- LocalCommunityVotingPlatform/ClientApp/src/components/Users.js
+++ LocalCommunityVotingPlatform/ClientApp/src/components/Users.js
@@ -1,13 +1,47 @@
 ﻿import React, { Component } from 'react';
 //import { UsersTable } from './UsersTable';
 import { UsersTable } from './UsersTable';
+import { UpdateUser } from './UpdateUser';
+
+import axios from 'axios'
+import { getJWTtoken } from '../helpers/jwtHandler'
 
 export class Users extends Component {
     static displayName = Users.name;
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showList: true,
+            userEmail: ''
+        }
+    }
+
+
+    ShowFormEdit = (userEmailFromButton) => {
+        this.setState({
+            showList: !this.state.showList,
+            userEmail: userEmailFromButton
+        })          
+        console.log(userEmailFromButton);
+    }
+
+
+    //Delete = (userEmailFromButton) => {
+        
+    //    axios.post('/api/DeleteUser', {
+    //        headers: {
+    //            Authorization: getJWTtoken()
+    //        },
+    //        Email: this.state.userEmail
+    //    }).then(res => this.forceUpdate());
+    //}
+
     render() {
         return <div style={{ marginTop: 30 }}>
-             <UsersTable/>
+            {this.state.showList ? <UsersTable ShowFormEdit={this.ShowFormEdit} Delete
+                ={this.Delete} /> : <UpdateUser ShowFormEdit={this.ShowFormEdit} />}
             </div>
     }
 }

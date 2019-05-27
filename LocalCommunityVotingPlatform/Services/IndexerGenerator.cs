52c33decf9e4f424d2c8ff6ef71a9c05e5c41a49
Łukasz Czepielik <koragrex@gmail.com>
2019-05-28 00:33:27 +0200

    Added IndexerGenerator helper class.

---
+++ LocalCommunityVotingPlatform/Services/IndexerGenerator.cs
@@ -0,0 +1,27 @@
+﻿using LocalCommunityVotingPlatform.DAL;
+using System;
+
+namespace LocalCommunityVotingPlatform.Services
+{
+    public class IndexerGenerator
+    {
+        private IDbOperations _context;
+
+        public IndexerGenerator()
+        {
+            _context = new DbOperations();
+        }
+
+        public string GenerateIndexer()
+        {
+            var Month = DateTime.UtcNow.Month;
+            var Year = DateTime.UtcNow.Year;
+
+            var Quantity = _context.GetResolutionsCountByMonth(Month);
+
+            string Indexer = $"{Quantity + 1}/{Month}/{Year}";
+
+            return Indexer;
+        }
+    }
+}

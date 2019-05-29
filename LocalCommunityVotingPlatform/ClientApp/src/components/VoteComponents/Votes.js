b8c7f7bc349433aadc2043446a5c5442536c0e5f
Łukasz Czepielik <koragrex@gmail.com>
2019-05-29 02:07:58 +0200

    Added Votes component.

---
+++ LocalCommunityVotingPlatform/ClientApp/src/components/VoteComponents/Votes.js
@@ -0,0 +1,32 @@
+﻿import React, { Component } from 'react';
+import { ActiveResolutionsList } from './ActiveResolutionsList';
+
+export class Votes extends Component {
+    static displayName = Votes.name;
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            resolutionId: '',
+            refreshNeeded: false,
+        }
+    }
+
+    RefreshComponent = () => {
+        this.setState({
+            refreshNeeded: !this.state.refreshNeeded
+        });
+    }
+
+    VoteForResolution = (singleId) => {
+
+    }
+
+    render() {
+        return (
+            <div style={{ marginTop: 30 }}>
+                <ActiveResolutionsList refreshNeeded={this.state.refreshNeeded} VoteForResolution={this.VoteForResolution} RefreshComponent={this.RefreshComponent} />
+            </div >
+        )
+    }
+}

c349cd0c2e6d4a811415de26317c32c31f53798c
Łukasz Czepielik <koragrex@gmail.com>
2019-05-29 02:07:48 +0200

    Added ActiveResolutionsList component.

---
+++ LocalCommunityVotingPlatform/ClientApp/src/components/VoteComponents/ActiveResolutionsList.js
@@ -0,0 +1,117 @@
+﻿import React, { Component } from 'react';
+import { MDBDataTable, MDBInput, MDBBtn } from 'mdbreact';
+import { getJWTtoken } from '../../helpers/jwtHandler'
+
+
+export class ActiveResolutionsList extends Component {
+    static displayName = ActiveResolutionsList.name;
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: {
+                columns: [
+                    {
+                        label: 'Indeks',
+                        field: 'indexer',
+                        sort: 'asc',
+                        width: 100
+                    },
+                    {
+                        label: 'Tytuł',
+                        field: 'title',
+                        sort: 'asc',
+                        width: 300
+                    },
+                    {
+                        label: 'Treść',
+                        field: 'description',
+                        sort: 'asc',
+                        width: 700
+                    },
+                    {
+                        label: 'Data ważności',
+                        field: 'activeToVoteBeforeDate',
+                        sort: 'asc',
+                        width: 100
+                    },
+                    {
+                        label: 'Głosuj',
+                        field: 'vote',
+                        sort: 'asc',
+                        width: 100
+                    }
+                ],
+                rows: []
+            }
+        }
+    }
+
+    componentDidUpdate = async () => {
+        if (this.props.refreshNeeded == true) {
+            await this.downloadActiveResolutions();
+            await this.props.RefreshComponent();
+        }
+    }
+
+    componentDidMount = () => {
+        this.downloadActiveResolutions();
+    }
+
+    downloadActiveResolutions() {
+        let data = Object.assign({}, this.state.data);
+
+        fetch('api/Resolution/GetActiveResolutions', {
+            headers: {
+                Authorization: getJWTtoken()
+            }
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+                data.rows = [];
+
+                for (let i = 0; i < result.length; i++) {
+                    let singleId = result[i].id;
+                    let resolutionCredentials = result[i].indexer + " " + result[i].title;
+
+                    console.log(singleId);
+                    console.log(resolutionCredentials);
+
+                    var object = {
+                        indexer: result[i].indexer,
+                        title: result[i].title,
+                        description: result[i].description,
+                        activeToVoteBeforeDate: result[i].activeToVoteBeforeDate,
+                        vote: '',
+                    }
+
+                    console.log(result);
+
+                    data.rows.push(object);
+
+                    console.log(data);
+
+                    data.rows[i].vote = <MDBBtn label="Vote" className="button tiny success" onClick={() => this.props.VoteForResolution(singleId)} style={{ marginBottom: 0 }}>Głosuj</MDBBtn>
+                }
+
+                this.setState({ data });
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <MDBDataTable
+                    responsive
+                    striped
+                    bordered
+                    hover
+                    data={this.state.data}
+                />
+            </div>
+        );
+    }
+}
+
+export default ActiveResolutionsList;

bc308f3aec71c0ec4cc6da41c843614f632262bb
Łukasz Czepielik <koragrex@gmail.com>
2019-05-30 23:14:13 +0200

    Added validation attributes to AddResolutionViewModel and UserViewModel.

--- LocalCommunityVotingPlatform/ViewModels/AddResolutionViewModel.cs
+++ LocalCommunityVotingPlatform/ViewModels/AddResolutionViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace LocalCommunityVotingPlatform.ViewModels
 {
     public class AddResolutionViewModel
     {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Pole \"Tytuł\" powinno zawierać minimum 3 znaki")]
+        [DataType(DataType.Text)]
         public string Title { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Pole \"Opis\" powinno zawierać minimum 3 znaki")]
+        [DataType(DataType.Text)]
         public string Description { get; set; }
+
+        [Required]
         public DateTime ActiveToVoteBeforeDate { get; set; }
     }
 }
--- LocalCommunityVotingPlatform/ViewModels/UserViewModel.cs
+++ LocalCommunityVotingPlatform/ViewModels/UserViewModel.cs
@@ -1,10 +1,24 @@
-﻿namespace LocalCommunityVotingPlatform.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocalCommunityVotingPlatform.ViewModels
 {
     public class UserViewModel
     {
+        [Required(ErrorMessage = "Pole \"Adres email \" jest wymagane")]
+        [EmailAddress(ErrorMessage = "Pole \"Adres email \" nie zawiera poprawnie wprowadzonego adresu")]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Imię powinno zawierać od 3 do 100 znaków.")]
+        [DataType(DataType.Text)]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Pole \"Nazwisko\" powinno zawierać od 3 do 100 znaków.")]
+        [DataType(DataType.Text)]
         public string LastName { get; set; }
+
+        [Required]
         public string Role { get; set; }
 
         public string Edit { get; set; }

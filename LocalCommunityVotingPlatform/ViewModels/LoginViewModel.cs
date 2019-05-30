2a57d92328d60ea9539923925805082c3eb3fc77
Łukasz Czepielik <koragrex@gmail.com>
2019-05-30 23:02:27 +0200

    Added DataAnnotations for Login and Register ViewModels.

--- LocalCommunityVotingPlatform/ViewModels/LoginViewModel.cs
+++ LocalCommunityVotingPlatform/ViewModels/LoginViewModel.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace LocalCommunityVotingPlatform.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Pole \"Adres email \" jest wymagane")]
+        [EmailAddress(ErrorMessage = "Pole \"Adres email \" nie zawiera poprawnie wprowadzonego adresu")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Pole \"Hasło\" jest wymagane")]
+        [MinLength(8, ErrorMessage ="Hasło nie może być krótsze niż 8 znaków")]
         public string Password { get; set; }
     }
 }
--- LocalCommunityVotingPlatform/ViewModels/RegisterViewModel.cs
+++ LocalCommunityVotingPlatform/ViewModels/RegisterViewModel.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LocalCommunityVotingPlatform.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage = "Pole \"Adres email \" jest wymagane")]
+        [EmailAddress(ErrorMessage = "Pole \"Adres email \" nie zawiera poprawnie wprowadzonego adresu")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Pole \"Hasło\" jest wymagane")]
+        [MinLength(8, ErrorMessage = "Hasło nie może być krótsze niż 8 znaków")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Pole \"Potwierdź hasło\" jest wymagane")]
+        [Compare("Password", ErrorMessage = "Wprowadzone hasła różnią się.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Imię powinno zawierać od 3 do 100 znaków.")]
+        [DataType(DataType.Text)]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Pole \"Nazwisko\" powinno zawierać od 3 do 100 znaków.")]
+        [DataType(DataType.Text)]
         public string LastName { get; set; }
 
-
         public ICollection<string> AvailableRoles { get; set; }
+
+        [Required]
         public string SelectedRole { get; set; }
     }
 }

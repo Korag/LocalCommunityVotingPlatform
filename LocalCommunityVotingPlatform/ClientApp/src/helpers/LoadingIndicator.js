ab14434f79eb04704301963351b967f3ea47c93e
Łukasz Czepielik <koragrex@gmail.com>
2019-06-26 23:58:52 +0200

    Added LoadingIndicator helper method.

---
+++ LocalCommunityVotingPlatform/ClientApp/src/helpers/LoadingIndicator.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import { usePromiseTracker } from "react-promise-tracker";
+import Loader from 'react-loader-spinner';
+
+const LoadingIndicator = (props) => {
+    const { promiseInProgress } = usePromiseTracker();
+
+    return (
+        promiseInProgress &&
+        <Loader style="loader" type="Circles" color="#534F9D" height="100" width="100" />
+    )
+}
+
+export default LoadingIndicator;

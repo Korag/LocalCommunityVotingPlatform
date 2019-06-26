3a624569c40c10bb3eac8fd26084ae70fc7a9863
Łukasz Czepielik <koragrex@gmail.com>
2019-06-27 01:49:24 +0200

    Added DisplayUserCredentials ViewModel.

---
+++ LocalCommunityVotingPlatform/ViewModels/DisplayUserCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace LocalCommunityVotingPlatform.ViewModels
+{
+    public class DisplayUserCredentials
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
